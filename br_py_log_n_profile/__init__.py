e11b6e69b2d83dc5925694adf16edffb9ff7db47
brais-laptop-wsl <brais-laptop-wsl@parshan.net>
2026-09-20 17:17:52 +0330

    feat(br_py_log_n_profile): expose NOT_TESTED in package root
    Expose the NOT_TESTED constant from the package root to allow easier
    access for users of the library.
    
    Also updates the br_pre_commit submodule to a newer commit.

--- br_py_log_n_profile/__init__.py
+++ br_py_log_n_profile/__init__.py
@@ -1,6 +1,7 @@
 """Reusable logging/profiling/tracing package extracted from Forecasting-sidecar3."""
 
 from br_py_log_n_profile.do_log import configure, get_ray_id, log, log_d, log_e, log_exception, log_i, log_w, set_ray_id
+from br_py_log_n_profile.do_log.log_it import NOT_TESTED
 from br_py_log_n_profile.profiling import profile_it
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "log_w",
     "profile_it",
     "set_ray_id",
+    "NOT_TESTED",
 ]
